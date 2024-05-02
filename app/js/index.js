4e6298724a8e01c7181cbefbb3e7a7dabeaf95b4
Shadman Ahmed <shadmanrashik@gmail.com>
2024-05-01 22:24:47 -0400

    Add files via upload

---
+++ app/js/index.js
@@ -0,0 +1,46 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+const navbar = document.getElementById("heronav");
+const mainNav = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if (window.scrollY >= sticky) {
+		mainNav.classList.add("sticky")
+	} else {
+		mainNav.classList.remove("sticky");
+	}
+}
+
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const nav = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function (e) {
+	e.preventDefault();
+
+	if (nav.classList.contains('open')) { // Close Hamburger Menu
+		body.classList.remove('noscroll');
+		nav.classList.remove('open');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
+
+	}
+	else { // Open Hamburger Menu
+		body.classList.add('noscroll');
+		nav.classList.add('open');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
+	}
+});
